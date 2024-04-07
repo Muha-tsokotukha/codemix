25260c5aa9be88d7cb66873ee4c8a85083070410
Mukhit Home <vinniedrew7@gmail.com>
2024-04-07 21:21:24 +0500

    feat: chat route

--- src/widgets/ChatList/index.tsx
+++ src/widgets/ChatList/index.tsx
@@ -5,8 +5,8 @@ export function ChatList() {
   const t = useTranslations('HomePage');
 
   return (
-    <aside className="md:max-w-96 xl:max-w-lg">
-      <section className="border-r border-gray flex items-center py-5 pl-10 pr-8 sm:h-[92px] border-b border-gray">
+    <aside className="md:w-[375px] xl:w-[512px]">
+      <section className="border-r border-gray flex items-center py-5 pl-6 sm:pl-10  pr-8 sm:h-[92px] border-b border-gray">
         <p className="text-primary font-semibold text-xl">{t('messages')}(3)</p>
       </section>
 
